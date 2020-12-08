756e00d357212e19a09dd0010202f9f9eb131d4d
ruucm <ruucm.a@gmail.com>
2020-12-08 15:00:45 -0800

    add images & upgrade

--- pages/freelance-works.js
+++ pages/freelance-works.js
@@ -1,9 +1,16 @@
 import Link from "next/link"
+import Image from "next/image"
 
 export default function FreelanceWorks() {
   return (
     <div>
       <div>Freelance Works</div>
+      <Image
+        src="/assets/portfolio-1.jpeg"
+        alt="Picture of the author"
+        width={500}
+        height={500}
+      />
 
       <div>
         Back to{" "}
