0be37d229054a3412d9fe45731e8d27e1c16a8b5
ruucm <ruucm.a@gmail.com>
2020-12-11 21:33:06 -0800

    Deploy Next.js to gh-pages

--- out/_next/static/chunks/webpack.js
+++ out/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "a866580d9cbbef4fefd4";
+/******/ 	var hotCurrentHash = "2a7d266fccc657d15665";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
