7e3fc5b3f42b52b78af3559a74effd50b626cee6
ruucm <ruucm.a@gmail.com>
2020-12-14 12:53:04 -0800

    up

--- components/card.js
+++ components/card.js
@@ -2,42 +2,40 @@ import Link from "next/link"
 
 export function Card({ title, duration, outlink, link }) {
   return (
-    <>
-      <div
+    <div
+      style={{
+        border: "1px solid black",
+        marginBottom: 10,
+        padding: "30px 15px",
+      }}
+    >
+      <h4
         style={{
-          border: "1px solid black",
-          marginBottom: 10,
-          padding: "30px 15px",
-          display: "inline-block",
+          marginTop: 0,
         }}
       >
-        <h4
-          style={{
-            marginTop: 0,
-          }}
-        >
-          {title}
-          {duration && ` (${duration})`}
-        </h4>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras cursus
+        {title}
+        {duration && ` (${duration})`}
+      </h4>
+      <p>
+        {Math.random() > 0.3
+          ? `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras cursus
           risus eu nisi finibus egestas. Vivamus nec pharetra urna. Mauris enim
           tortor, lacinia vel euismod iaculis, congue quis augue. In
-          pellentesque mauris vel risus tempor, non porttitor arcu porttitor.
-        </p>
-        {link && (
-          <Link href={link}>
-            <a>More</a>
-          </Link>
-        )}
-        {!link && (
-          <a href={outlink} target="_blank">
-            More (outlink)
-          </a>
-        )}
-      </div>
-
-      <br />
-    </>
+          pellentesque mauris vel risus tempor, non porttitor arcu porttitor.`
+          : `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras cursus
+          risus eu nisi finibus egestas.`}
+      </p>
+      {link && (
+        <Link href={link}>
+          <a>More</a>
+        </Link>
+      )}
+      {!link && (
+        <a href={outlink} target="_blank">
+          More (outlink)
+        </a>
+      )}
+    </div>
   )
 }
