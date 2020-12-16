d2ecb4895129d301f48e60e584f2b3f50a3fd313
ruucm <ruucm.a@gmail.com>
2020-12-16 11:36:56 -0800

    rm unusings

--- pages/portfolio/[id].page.js
+++
@@ -1,9 +0,0 @@
-import { useRouter } from "next/router"
-import { Layout } from "../../components"
-
-export default function ClubPost(props) {
-  const router = useRouter()
-  const { id } = router.query
-
-  return <Layout>{id}</Layout>
-}
