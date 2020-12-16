b21a262d3f6cc5912b01d2c8db6828981c0e4c53
ruucm <ruucm.a@gmail.com>
2020-12-16 11:34:11 -0800

    update framer-korea

--- pages/portfolio/framer-korea.page.js
+++ pages/portfolio/framer-korea.page.js
@@ -28,36 +28,16 @@ export default function HarborSchoolPage(props) {
             color: "blue",
           }}
         >
-          Education Content Experience
+          2019 Framer Korea Meetup Website
         </h4>
-        <div
-          style={{
-            display: "grid",
-            gap: 30,
-          }}
-        >
-          <PortfolioSectionCard
-            img={require("../../assets/problem-youtube.png")}
-            title="[Problems] - Bad Education Content Experience"
-            description={`I learn programming and design by myself with youtube videos and blog
-        posts. But as a learner's view, there are solid problem on both side.
-        On video side, it is always difficult to jump the point directly you
-        want. If you find a just example line of code, watching video could be
-        terrible experiences. On post side, it is easy to find information but
-        sometimes post author could skip contents that makes students take a
-        ton of times for finding why their project don't excuted like the
-        post.`}
-            // grayscale
-          />
-          <PortfolioSectionCard
-            img={require("../../assets/solution-video-explorer.gif")}
-            title="[Solution] - Video Explorer"
-            description={`So I started develop a new UI called "Video Explorer". I combines good
-      points of video and post at the same time. It includes whole proccess
-      of tutorial to make the results, and user can find information quickly
-      like the blog posts.`}
-          />
-        </div>
+        <PortfolioSectionCard
+          img={require("../../assets/framer-korea-2019.png")}
+          title=""
+          description={`I developed whole websites using Gatsby.js with the other junior developer. I used Framer to communicate with a designer for motion.
+
+          It also supports bilingual en/ko without losing the website's speed.
+          `}
+        />
 
         <h4
           style={{
@@ -66,32 +46,14 @@ export default function HarborSchoolPage(props) {
             color: "blue",
           }}
         >
-          Internal Student Management System
+          2020 Framer Korea Meetup Website
         </h4>
-        <div
-          style={{
-            display: "grid",
-            gap: 30,
-          }}
-        >
-          <PortfolioSectionCard
-            img={require("../../assets/problem-students-management.png")}
-            title="[Problems] - Seperated CRM and Student Managements"
-            description={`I had to offer CMS(Content Management System) with student's dashboard.`}
-            // grayscale
-            // alignRight
-          />
-
-          <PortfolioSectionCard
-            img={require("../../assets/solution-students-management.png")}
-            title="[Solution] - Student Management System"
-            description={`And I need to collect latest resources for students. But we had only one
-            junior developer and me with limited time.
-            I started to find the fastest and reliable method for that
-            problem.`}
-            // alignRight
-          />
-        </div>
+        <PortfolioSectionCard
+          img={require("../../assets/framer-korea-2020.png")}
+          title=""
+          description={`
+          I developed whole websites using Gatsby.js. I prototyped motions of the website on Framer and moved it to the React development environment without hassle.`}
+        />
       </AnimateSharedLayout>
     </Layout>
   )
