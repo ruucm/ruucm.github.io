967202b522d3902929470a8ea5e727f0e5b3af57
ruucm <ruucm.a@gmail.com>
2020-12-15 19:30:24 -0800

    update styles

--- pages/portfolio/harbor-school.js
+++ pages/portfolio/harbor-school.js
@@ -36,16 +36,18 @@ export default function HarborSchoolPage(props) {
         </a>
       </p>
       <AnimateSharedLayout>
-        <div
+        <h3
           style={{
-            display: "grid",
-            gap: 30,
+            marginTop: 62,
+            textAlign: "right",
           }}
         >
-          <PortfolioSectionCard
-            img={require("../../assets/problem-youtube.png")}
-            title="[Problems] - Education Content Experience"
-            description={`I learn programming and design by myself with youtube videos and blog
+          Education Content Experience
+        </h3>
+        <PortfolioSectionCard
+          img={require("../../assets/problem-youtube.png")}
+          title="[Problems] - Education Content Experience"
+          description={`I learn programming and design by myself with youtube videos and blog
         posts. But as a learner's view, there are solid problem on both side.
         On video side, it is always difficult to jump the point directly you
         want. If you find a just example line of code, watching video could be
@@ -53,36 +55,42 @@ export default function HarborSchoolPage(props) {
         sometimes post author could skip contents that makes students take a
         ton of times for finding why their project don't excuted like the
         post.`}
-            grayscale
-          />
-
-          <PortfolioSectionCard
-            img={require("../../assets/solution-video-explorer.gif")}
-            title="[Solution] - Video Explorer"
-            description={`So I started develop a new UI called "Video Explorer". I combines good
+          grayscale
+        />
+        <PortfolioSectionCard
+          img={require("../../assets/solution-video-explorer.gif")}
+          title="[Solution] - Video Explorer"
+          description={`So I started develop a new UI called "Video Explorer". I combines good
       points of video and post at the same time. It includes whole proccess
       of tutorial to make the results, and user can find information quickly
       like the blog posts.`}
-          />
+        />
 
-          <PortfolioSectionCard
-            img={require("../../assets/problem-students-management.png")}
-            title="[Problems] - Education Content Experience"
-            description={`I had to offer CMS(Content Management System) with student's dashboard.`}
-            grayscale
-            alignRight
-          />
+        <h3
+          style={{
+            marginTop: 62,
+            textAlign: "right",
+          }}
+        >
+          Internal Student Management System
+        </h3>
+        <PortfolioSectionCard
+          img={require("../../assets/problem-students-management.png")}
+          title="[Problems] - Seperated CRM and Student Managements"
+          description={`I had to offer CMS(Content Management System) with student's dashboard.`}
+          grayscale
+          // alignRight
+        />
 
-          <PortfolioSectionCard
-            img={require("../../assets/solution-students-management.png")}
-            title="[Solution] - Student Management System"
-            description={`And I need to collect latest resources for students. But we had only one
+        <PortfolioSectionCard
+          img={require("../../assets/solution-students-management.png")}
+          title="[Solution] - Student Management System"
+          description={`And I need to collect latest resources for students. But we had only one
             junior developer and me with limited time.
             I started to find the fastest and reliable method for that
             problem.`}
-            alignRight
-          />
-        </div>
+          // alignRight
+        />
       </AnimateSharedLayout>
     </Layout>
   )
