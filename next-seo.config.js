6bfdef174c912a7963a4cab9fd99a83820097012
ruucm <ruucm.a@gmail.com>
2021-05-24 21:33:30 +0900

    break: domain changes (ruucm.work)

--- next-seo.config.js
+++ next-seo.config.js
@@ -4,16 +4,16 @@ const description = `yongmin ruucm is an experienced front-end developer with a 
 const SEO = {
   title,
   description,
-  canonical: "https://ruucm.me",
+  canonical: "https://ruucm.work",
   openGraph: {
     type: "website",
     locale: "en_IE",
-    url: "https://ruucm.me",
+    url: "https://ruucm.work",
     title,
     description,
     images: [
       {
-        url: "https://ruucm.me/static/images/brand/og.png",
+        url: "https://ruucm.work/static/images/brand/og.png",
         alt: title,
         width: 1280,
         height: 720,
