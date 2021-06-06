6642fbd6c531da58a791c2cbf2251747d6756965
ruucm <ruucm.a@gmail.com>
2021-06-06 18:25:34 +0900

    Deploy Vite.js to gh-pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react"
+import ReactDOM from "react-dom"
+import "./index.css"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+import("./imports.js").then(async (m) => {
+  const App = (await import("./App")).default
+
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById("root")
+  )
+})
